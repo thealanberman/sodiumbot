41867dfbc2f490041f81b2b25a6cbada7020baee
Al <thealanberman@gmail.com>
2016-10-22 22:33:25 -0700

    made sodiumbot more robust

--- install.sh
+++ install.sh
@@ -1,3 +1,9 @@
 #!/bin/bash
 
+# prompt
+read -e -s -p "Install requests library and create sodiumbot.zip [y/n]? " -n 1 -r
+[[ ! $REPLY =~ ^[Yy]$ ]] && { echo "Nope? Okay."; exit; }
+
 pip install requests -t . --upgrade
+zip -r9 sodiumbot.zip *
+echo "upload sodiumbot.zip to your AWS Lambda function."
